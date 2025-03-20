f0323e17a56ea1d9cbc1298ecb2d25badc9e796f
marcos7 <choqueerick249@gmail.com>
2025-03-20 15:09:33 -0400

    20-03-2025

---
+++ DOM_AVANZADO/componentes/script.js
@@ -0,0 +1,11 @@
+import Form from "./formulario.js";
+import taskTable from "./tabla.js";
+import cards from "./cards.js";
+
+(() => {
+    Form.setDatos((task) => {
+        taskTable.addTask(task);
+        cards.update();
+    });
+})();
+//actualizado
